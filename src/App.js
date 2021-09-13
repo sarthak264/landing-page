6ff1f594453e8a3f531b2a58e20bac32deca56ea
sarthak264 <sarthaksharm372@gmail.com>
2021-09-13 13:00:40 +0530

    navbar made

--- src/App.js
+++ src/App.js
@@ -1,6 +1,17 @@
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+
 function App() {
   return (
-    <h1>Style Two</h1>
+    <div className="page_wrapper">
+      <div className="container">
+        <navbar className="navbar_container">
+          <a href="/">
+            <img src="./images/logo-white.svg" alt="logo" />
+          </a>
+          <button className="buy_btn mon">Buy Now</button>
+        </navbar>
+      </div>
+    </div>
   );
 }
 
