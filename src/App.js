fb0f58268d4255c9cf9b7c643b13263e79347cd8
sarthak264 <sarthaksharm372@gmail.com>
2021-09-13 18:34:46 +0530

    header done

--- src/App.js
+++ src/App.js
@@ -4,12 +4,33 @@ function App() {
   return (
     <div className="page_wrapper">
       <div className="container">
+        {/* Navbar */}
         <navbar className="navbar_container">
           <a href="/">
             <img src="./images/logo-white.svg" alt="logo" />
           </a>
           <button className="buy_btn mon">Buy Now</button>
         </navbar>
+        {/* Header */}
+        <header className="header text-center">
+          <div className="row justify-content-center">
+            <div className="col-lg-10 col-xl-8">
+              <h1 className="heading mon mb-3 mb-sm-4">
+                Harnessing the power of müse.
+              </h1>
+              <h2 className="subheading mon">
+                Design, prototype, collaborate, and bring your ideas to life
+                with the world.
+              </h2>
+            </div>
+          </div>
+          <button className="pricing_btn mon">See Pricing</button>
+          <img
+            src="./images/saas-banner.jpg"
+            alt="banner"
+            className="header_banner"
+          />
+        </header>
       </div>
     </div>
   );
