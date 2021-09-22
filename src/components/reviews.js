e391dc0672a44547244e3eae965a9e9f73158a40
sarthak264 <sarthaksharm372@gmail.com>
2021-09-22 21:54:44 +0530

    made the list of reviews

---
+++ src/components/reviews.js
@@ -0,0 +1,30 @@
+export const review = [
+    {
+        company: 'Microsoft',
+        name:'Dameon Peterson',
+        post: 'Lead Designer',
+        image: './images/microsoft-colored.svg',
+        detail: 'Great customer support and fantastic product. I have never had a personalized video demo emailed to me! The product is seamless too, fully responsive, made with a lot of care and attention to detail 1.'
+    },
+    {
+        company: 'Microsoft',
+        name:'Dameon Peterson',
+        post: 'Lead Designer',
+        image: './images/microsoft-colored.svg',
+        detail: 'Great customer support and fantastic product. I have never had a personalized video demo emailed to me! The product is seamless too, fully responsive, made with a lot of care and attention to detail 2.'
+    },
+    {
+        company: 'Microsoft',
+        name:'Dameon Peterson',
+        post: 'Lead Designer',
+        image: './images/microsoft-colored.svg',
+        detail: 'Great customer support and fantastic product. I have never had a personalized video demo emailed to me! The product is seamless too, fully responsive, made with a lot of care and attention to detail 3.'
+    },
+    {
+        company: 'Microsoft',
+        name:'Dameon Peterson',
+        post: 'Lead Designer',
+        image: './images/microsoft-colored.svg',
+        detail: 'Great customer support and fantastic product. I have never had a personalized video demo emailed to me! The product is seamless too, fully responsive, made with a lot of care and attention to detail 4.'
+    },
+]
